48c1f8eba75066d3087213f7a780d1d28e83abc3
Dr-Lepic <mbmahbub007@gmail.com>
2025-03-27 08:13:16 +0600

    added a dateAndTime formatter

--- src/Utils.java
+++ src/Utils.java
@@ -3,7 +3,11 @@ import java.util.Date;
 
 public class Utils {
     public static String formatDate(Date date) {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
+        return formatter.format(date);
+    }
+    public static String formatDateAndTime(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy HH:mm");
         return formatter.format(date);
     }
 }
