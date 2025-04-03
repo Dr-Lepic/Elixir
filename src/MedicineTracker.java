076864e638ea285fecf0e0da8c520933b32e2e6f
Dr-Lepic <mbmahbub007@gmail.com>
2025-04-03 10:44:02 +0600

    Created MedicineTracker and implemented

---
+++ src/MedicineTracker.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class MedicineTracker {
+    private static class Medicine {
+        String name;
+        String dose;
+        String schedule;
+        List<String> days;
+
+        Medicine(String name, String dose, String schedule, List<String> days) {
+            this.name = name;
+            this.dose = dose;
+            this.schedule = schedule;
+            this.days = days;
+        }
+    }
+
+    private List<Medicine> medicines = new ArrayList<>();
+
+    public void addMedicine(String name, String dose, String schedule, List<String> days) {
+        medicines.add(new Medicine(name, dose, schedule, days));
+    }
+
+    public void showDailySchedule(String day) {
+        System.out.println("Medications for " + day + ":");
+
+        for (Medicine medicine : medicines) {
+            if (medicine.days.contains(day)) {
+                System.out.println("- " + medicine.name + " " + medicine.dose + " at " + medicine.schedule);
+            }
+        }
+    }
+
+    public void showWeeklySummary() {
+        Map<String, Integer> weeklySummary = new HashMap<>();
+
+        for (Medicine medicine : medicines) {
+            for (String day : medicine.days) {
+                weeklySummary.merge(day + " " + medicine.name, 1, Integer::sum);
+            }
+        }
+
+        System.out.println("Weekly Medication Summary:");
+        weeklySummary.forEach((k, v) ->
+                System.out.println("- " + k + ": " + v + " doses"));
+    }
+}
